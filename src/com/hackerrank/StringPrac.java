085f0684f09237c7cae81d04f257e8023d9f2f7a
radoncreep <radoncreep@gmail.com>
2021-04-05 11:32:07 +0100

    first

---
+++ src/com/hackerrank/StringPrac.java
@@ -0,0 +1,23 @@
+package com.hackerrank;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class StringPrac {
+    public static void main(StringPrac[] args) {
+
+    }
+
+}
+
+//    NumberFormat uk = NumberFormat.getCurrencyInstance(Locale.US);
+//    NumberFormat china = NumberFormat.getCurrencyInstance(Locale.CHINA);
+//    NumberFormat indiaCur = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+//
+//    double price = 10.90;
+//
+//    NumberFormat formatter = NumberFormat.getCurrencyInstance(new Locale("en", "IN"));
+//
+//    String moneyString = formatter.format(price);
+//
+//        System.out.println(moneyString);
